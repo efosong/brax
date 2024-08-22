6596ed9b2972c29e3fdb05ce2c369b23eb5cfc28
Leo <lh.from.kb@gmail.com>
2024-08-22 16:10:42 +0100

    adding armanipulation task

--- brax/envs/__init__.py
+++ brax/envs/__init__.py
@@ -35,6 +35,7 @@ from brax.envs import panda
 from brax.envs import wheelchair
 from brax.envs import scratchitch
 from brax.envs import bedbathing
+from brax.envs import armmanipulation
 from brax.envs.base import Env, PipelineEnv, State, Wrapper
 from brax.envs.wrappers import training
 
@@ -56,6 +57,7 @@ _envs = {
     "wheelchair": wheelchair.Wheelchair,
     "scratchitch": scratchitch.ScratchItch,
     "bedbathing": bedbathing.BedBathing,
+    "armmanipulation": armmanipulation.ArmManipulation,
 }
 
 
