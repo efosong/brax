422a6c4a41b613a2f9957867502629823b0c9c10
Elle Miller <elle.miller@ed.ac.uk>
2024-11-27 16:49:58 +0000

    updating solver iteracions

--- brax/envs/scratchitch.py
+++ brax/envs/scratchitch.py
@@ -64,7 +64,7 @@ class ScratchItch(PipelineEnv):
                 {
                     "opt.solver": mujoco.mjtSolver.mjSOL_NEWTON,
                     "opt.disableflags": mujoco.mjtDisableBit.mjDSBL_EULERDAMP,
-                    "opt.iterations": 1,
+                    "opt.iterations": 4,
                     "opt.ls_iterations": 4,
                     "opt.timestep": 0.01
                 }
@@ -184,9 +184,9 @@ class ScratchItch(PipelineEnv):
             robo_obs["tool_position"],
             robo_obs["tool_orientation"],
             robo_obs["distance_to_target"].reshape((3,)),
-            # robo_obs["target_position"],
-            # robo_obs["human_uarm_pos"],
-            # robo_obs["human_larm_pos"],
+            robo_obs["target_position"],
+            robo_obs["human_uarm_pos"],
+            robo_obs["human_larm_pos"],
             robo_obs["force_on_tool"].reshape((6,)),
             robo_obs["robo_joint_angles"],
             robo_obs["robo_joint_vel"],
@@ -238,9 +238,9 @@ class ScratchItch(PipelineEnv):
             robo_obs["tool_position"],
             robo_obs["tool_orientation"],
             robo_obs["distance_to_target"].reshape((3,)),
-            # robo_obs["target_position"],
-            # robo_obs["human_uarm_pos"],
-            # robo_obs["human_larm_pos"],
+            robo_obs["target_position"],
+            robo_obs["human_uarm_pos"],
+            robo_obs["human_larm_pos"],
             robo_obs["force_on_tool"].reshape((6,)),
             robo_obs["robo_joint_angles"],
             robo_obs["robo_joint_vel"],
@@ -273,7 +273,7 @@ class ScratchItch(PipelineEnv):
         
         # less than 1cm for contact
         in_contact = (dist < 0.05).astype(float)
-        force_threshold = (scratcher_force_normal > 10.0).astype(float)
+        force_threshold = ((scratcher_force_normal > 10.0) & (scratcher_force_normal < 50.0)).astype(float)
         # r_scratching = in_contact * scratcher_force #force_threshold
         r_scratching = in_contact * force_threshold
         # Chosen Boltzmann-like reward functions for scratcher speed and force, but we could swap with alternatives.
@@ -288,7 +288,7 @@ class ScratchItch(PipelineEnv):
         ctrl_cost = -jp.sum(jp.square(robo_obs["robo_joint_vel"]))
 
         # sum rewards
-        reward = self._dist_reward_weight*r_dist + self._ctrl_cost_weight*ctrl_cost + self._scratching_reward_weight*r_scratching
+        reward = self._dist_reward_weight*r_dist  + self._ctrl_cost_weight*ctrl_cost + self._scratching_reward_weight*r_scratching
 
         done = 0.0
 
@@ -335,8 +335,8 @@ class ScratchItch(PipelineEnv):
         distance_to_target = target_position - tool_position
 
         # human pos - TODO: see if this is useful/normalise/express differently
-        # human_uarm_pos = pipeline_state.xpos[self.human_tuarm_idx]
-        # human_larm_pos = pipeline_state.xpos[self.human_tlarm_idx]
+        human_uarm_pos = pipeline_state.xpos[self.human_tuarm_idx]
+        human_larm_pos = pipeline_state.xpos[self.human_tlarm_idx]
 
         # normalise joint angles to range [0, 1], then [-1, 1]
         robo_joint_angles = pipeline_state.qpos[self.panda_joint_id_start:self.panda_joint_id_end]
@@ -355,9 +355,9 @@ class ScratchItch(PipelineEnv):
             "tool_position": tool_position,
             "tool_orientation": tool_orientation,
             "distance_to_target": distance_to_target,
-            # "target_position": target_position,
-            # "human_uarm_pos": human_uarm_pos,
-            # "human_larm_pos": human_larm_pos,
+            "target_position": target_position,
+            "human_uarm_pos": human_uarm_pos,
+            "human_larm_pos": human_larm_pos,
             "force_on_tool": force_on_tool,
             "robo_joint_angles": normalised_robo_joint_angles,
             "robo_joint_vel": robo_joint_vel
